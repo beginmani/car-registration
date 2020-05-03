6ae1fc2dc0c5d71ca563879edb1cf6f326689968
Vanshita99 <vgarg_be16@thapar.edu>
2020-05-03 11:15:10 +0530

    small errors

--- src/Components/FirstCarPage.js
+++ src/Components/FirstCarPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Image, Button, Card, Col } from "react-bootstrap";
+import { Image, Button, Card, Col, Nav, Row } from "react-bootstrap";
 import Register from "./Register";
 //import img1 from " ../images/2004_Porsche_911_Carrera_type_997.jpg";
 import img2 from "../images/250px-Nissan_GT-R.jpg";
@@ -17,31 +17,67 @@ class FirstCarPage extends Component {
 
   render() {
     return (
-      <Col sm={3}>
-        <Card style={{ width: "30rem" }}>
+      //    <div>
+      //         <li>
+      //             {this.props.carinfo.name}
+      //         </li>
+      //         <li>
+      //             {this.props.carinfo.price}
+      //         </li>
+      //         <li>
+      //         <Image src={this.props.carinfo.img} fluid/>
+      //        </li>
+      //         <Button>See Details</Button>
+
+      //         <Button disabled={this.state.bookingStatus}  onClick={() => this.updateBookingStatus()}>Book Now</Button>
+      //
+
+      //   <Card style={{ width: "18rem" }}>
+      //     <Card.Img variant="top" src={this.props.carinfo.img} />
+      //     <Card.Body>
+      //       <Card.Title>{this.props.carinfo.name}</Card.Title>
+      //       <Card.Text>{this.props.carinfo.price}</Card.Text>
+      //       <Button variant="primary">See Details</Button>
+      //       <Button
+      //         disabled={this.state.bookingStatus}
+      //         onClick={() => this.updateBookingStatus()}
+      //       >
+      //         Book Now
+      //       </Button>
+      //     </Card.Body>
+      //   </Card>
+      <Col sm={4}>
+        <Card style={{ width: "25 rem" }}>
           <Card.Body>
-            <Card.Img variant="top" src={this.props.carinfo.img} />
+            <Card.Img variant="top" src={this.props.carinfo.img} rounded />
             <Card.Title>{this.props.carinfo.name}</Card.Title>
             <Card.Title>{this.props.carinfo.price}</Card.Title>
-
             <div>
-              <Button
-                variant="primary"
-                size="xs"
-                onClick={() => this.props.onView(this.props.carinfo)}
-              >
-                See Details
-              </Button>
-
-              <Button
-                variant="primary"
-                size="xs"
-                disabled={this.state.bookingStatus}
-                onClick={() => this.updateBookingStatus()}
-              >
-                {" "}
-                Book Now
-              </Button>
+              <Row>
+                <Col sm={6}>
+                  <button
+                    type="button"
+                    class="btn btn-primary btn-md"
+                    onClick={() => this.props.onView(this.props.carinfo)}
+                  >
+                    See details
+                  </button>{" "}
+                </Col>
+                <Col sm={6}>
+                  <Nav.Link href="/form">
+                    {" "}
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-md"
+                      disabled={this.state.bookingStatus}
+                      onClick={() => this.updateBookingStatus()}
+                    >
+                      {" "}
+                      Book Now
+                    </button>
+                  </Nav.Link>
+                </Col>
+              </Row>
             </div>
           </Card.Body>
         </Card>
