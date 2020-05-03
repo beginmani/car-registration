89c2976fa878d6d5bf19fc3166e24efec1dd2488
Vanshita99 <vgarg_be16@thapar.edu>
2020-05-03 06:16:28 +0530

    html

--- src/Components/CarList.js
+++ src/Components/CarList.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
-import FirstCarPage from './FirstCarPage';
-import data from './cars.json';
-import {Route, IndexRoute} from 'react-router';
-import * as ReactBootstrap  from "react-bootstrap";
-class  CarList extends Component {
-    constructor(){
+import React, { Component } from "react";
+import FirstCarPage from "./FirstCarPage";
+import data from "./cars.json";
+import { Route, IndexRoute } from "react-router";
+import * as ReactBootstrap from "react-bootstrap";
+import { Row } from "react-bootstrap";
+class CarList extends Component {
+  constructor() {
     super();
     this.state = {
-            cars: [],
-        }
-    }
+      cars: [],
+    };
+  }
 
-    componentWillMount(){
-        this.setState({ cars: data });
-    }
-   
+  componentWillMount() {
+    this.setState({ cars: data });
+  }
 
-    renderCars() {
-        return this.state.cars.map((carinfo,index) => {
-           return <ReactBootstrap.CardDeck>
-           <FirstCarPage key={index} carinfo={carinfo} />
-           </ReactBootstrap.CardDeck>
-        })
-      }
+  renderCars() {
+    return this.state.cars.map((carinfo, index) => {
+      return <FirstCarPage key={index} carinfo={carinfo} />;
+    });
+  }
 
-    render() {
-        
-        return (
-            <div>
-                {this.renderCars()}
-             </div>
-        );
-    }
+  render() {
+    return (
+      <div class="container">
+        <Row>{this.renderCars()}</Row>
+      </div>
+    );
+  }
 }
 
 export default CarList;
--- src/Components/FirstCarPage.js
+++ src/Components/FirstCarPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Image, Button, Card, Col,Nav } from "react-bootstrap";
+import { Image, Button, Card, Col, Nav, Row } from "react-bootstrap";
 import Register from "./Register";
 //import img1 from " ../images/2004_Porsche_911_Carrera_type_997.jpg";
 import img2 from "../images/250px-Nissan_GT-R.jpg";
@@ -46,26 +46,37 @@ class FirstCarPage extends Component {
       //       </Button>
       //     </Card.Body>
       //   </Card>
-      <Col sm={12}>
-        <Card style={{ width: "18rem" }}>
+      <Col sm={4}>
+        <Card style={{ width: "25 rem" }}>
           <Card.Body>
-            <Card.Img variant="top" src={this.props.carinfo.img} />
+            <Card.Img variant="top" src={this.props.carinfo.img} rounded />
             <Card.Title>{this.props.carinfo.name}</Card.Title>
             <Card.Title>{this.props.carinfo.price}</Card.Title>
-
             <div>
-            <Nav.Link  href="/seeDetail">   <Button variant="primary" size="xs">
-                See Details
-              </Button>{" "}</Nav.Link>
-           <Nav.Link  href="/form">   <Button
-                variant="primary"
-                size="xs"
-                disabled={this.state.bookingStatus}
-                onClick={() => this.updateBookingStatus()}
-              >
-                {" "}
-                Book Now
-              </Button></Nav.Link>
+              <Row>
+                <Col sm={6}>
+                  <Nav.Link href="/seeDetail">
+                    {" "}
+                    <button type="button" class="btn btn-primary btn-md">
+                      See details
+                    </button>{" "}
+                  </Nav.Link>
+                </Col>
+                <Col sm={6}>
+                  <Nav.Link href="/form">
+                    {" "}
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-md"
+                      disabled={this.state.bookingStatus}
+                      onClick={() => this.updateBookingStatus()}
+                    >
+                      {" "}
+                      Book Now
+                    </button>
+                  </Nav.Link>
+                </Col>
+              </Row>
             </div>
           </Card.Body>
         </Card>
