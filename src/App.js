04bb0bd506780b306491da73f8aa28c1329963ee
Bhavya Chopra <56370106+bhavya250198@users.noreply.github.com>
2020-05-03 13:43:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CarRegForm from "./Components/Form";
 import CarList from "./Components/CarList";
 import SeeDetails from "./Components/SeeDetails";
-
+import Welcome from './Components/Welcome';
 class App extends Component {
   render() {
     return (
       <div>
         <React.Fragment>
           <Router>
+            <Route path="/" exact component={Welcome}/>
             <Route path="/form" exact component={CarRegForm} />
             <Route path="/home" component={CarList} />
             <Route path="/seeDetail" component={SeeDetails} />
